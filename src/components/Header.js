208d3ce9990bce20a9eae327a6637cdc105ceffb
DavidGjorgjievski <david.gjorgjievski@students.finki.ukim.mk>
2025-04-05 17:11:44 +0200

    new header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef  } from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import '../styles/Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGear, faRightFromBracket, faBars   } from '@fortawesome/free-solid-svg-icons';
 
 
 function Header({ userInfo, fetchTopics = null, setIsFromLogo = null}) {// Accept userInfo as a prop
     const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+    const [isProfileMenuOpen, setProfileMenuOpen] = useState(false);
+    const profileRef = useRef(null);
 
     const toggleMobileMenu = () => {
         setMobileNavOpen(!isMobileNavOpen);
@@ -15,19 +19,35 @@ function Header({ userInfo, fetchTopics = null, setIsFromLogo = null}) {// Accep
     };
 
 useEffect(() => {
-    const logoImg = document.getElementById('logo-img');
-    const handleClick = () => {
-        setIsFromLogo(true);
-        if (fetchTopics) {
-            fetchTopics();
-        } else {
-            window.location.reload();
-        }
-    };
-    logoImg.addEventListener('click', handleClick);
-
-    return () => logoImg.removeEventListener('click', handleClick);
+  const logoImg = document.getElementById('logo-img');
+  const handleClick = () => {
+    if (typeof setIsFromLogo === 'function') {
+      setIsFromLogo(true);
+    }
+    if (fetchTopics) {
+      fetchTopics();
+    } else {
+      window.location.reload();
+    }
+  };
+  logoImg.addEventListener('click', handleClick);
+  return () => logoImg.removeEventListener('click', handleClick);
 }, [fetchTopics, setIsFromLogo]);
+
+
+ useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        profileRef.current &&
+        !profileRef.current.contains(event.target)
+      ) {
+        setProfileMenuOpen(false);
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () =>
+      document.removeEventListener('mousedown', handleClickOutside);
+  }, [profileRef]);
 
     return (
         <header>
@@ -41,6 +61,27 @@ useEffect(() => {
                             alt="Logo"
                         />
                     </div>
+
+
+                     {/* Mobile Navigation */}
+                    <div id="mobile-menu-toggle" className="mobile-menu-toggle" onClick={toggleMobileMenu}>
+                       <FontAwesomeIcon icon={faBars} className="hamburger-icon" />
+                    </div>
+                    <ul className={`nav-item-mobile ${isMobileNavOpen ? 'open' : ''}`} id="mobile-nav">
+                        <li><Link to="/">Почетна</Link></li>
+                        <li><Link to="/municipalities">Општини</Link></li>
+                         {userInfo.municipalityId && userInfo.municipalityId !== "Not Assigned" && (
+                            <li className={getActiveClass(`/municipalities/${userInfo.municipalityId}/sessions`)}>
+                                <Link to={`/municipalities/${userInfo.municipalityId}/sessions`}>Мои седници</Link>
+                            </li>
+                        )}
+                        <li style={{ display: userInfo.role === 'ROLE_ADMIN' ? 'block' : 'none' }}>
+                            <Link to="/admin-panel">Админ панел</Link>
+                        </li>
+                    </ul>
+
+                </div>
+
 
                     {/* Desktop Navigation */}
                     <ul className="nav-item" id="desktop-nav">
@@ -56,36 +97,22 @@ useEffect(() => {
                         </li>
                     </ul>
 
-                    {/* Mobile Navigation */}
-                    <div id="mobile-menu-toggle" className="mobile-menu-toggle" onClick={toggleMobileMenu}>
-                        <span className="hamburger-icon"></span>
-                        <span className="hamburger-icon"></span>
-                        <span className="hamburger-icon"></span>
-                    </div>
-                    <ul className={`nav-item-mobile ${isMobileNavOpen ? 'open' : ''}`} id="mobile-nav">
-                        <li><Link to="/">Почетна</Link></li>
-                        <li><Link to="/municipalities">Општини</Link></li>
-                         {userInfo.municipalityId && userInfo.municipalityId !== "Not Assigned" && (
-                            <li className={getActiveClass(`/municipalities/${userInfo.municipalityId}/sessions`)}>
-                                <Link to={`/municipalities/${userInfo.municipalityId}/sessions`}>Мои седници</Link>
-                            </li>
-                        )}
-                        <li style={{ display: userInfo.role === 'ROLE_ADMIN' ? 'block' : 'none' }}>
-                            <Link to="/admin-panel">Админ панел</Link>
-                        </li>
-                    </ul>
-                </div>
-
                 {/* User Profile and Logout Links */}
-                <ul className="nav-item">
-                    <li className="nav-image-item">
-                        <div>
-                            <img src={`data:image/jpeg;base64,${userInfo.image}`} className="header-image" alt="User Profile" />
+                    <div ref={profileRef} className="profile-menu-wrapper">
+                        <img
+                            src={`data:image/jpeg;base64,${userInfo.image}`}
+                            className="header-image"
+                            alt="User Profile"
+                            onClick={() => setProfileMenuOpen((open) => !open)}
+                        />
+                        {isProfileMenuOpen && (
+                            <div className="profile-dropdown">
+                            <p className="fw-bold">{userInfo.name} {userInfo.surname}</p>
+                            <Link to="/profile"> <FontAwesomeIcon icon={faGear} /> Поставки </Link>
+                            <Link to="/logout"> <FontAwesomeIcon icon={faRightFromBracket} /> Одјави се</Link>
+                            </div>
+                        )}
                         </div>
-                    </li>
-                    <li className='nav-ect'><Link to="/profile">{userInfo.name} {userInfo.surname}</Link></li>
-                    <li className='nav-ect'><Link to="/logout">Одјави се </Link></li>
-                </ul>
             </nav>
         </header>
     );
