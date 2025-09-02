2e96ca7c327b179a5531c9f489cc9734829c665c
DavidGjorgjievski <david.gjorgjievski@students.finki.ukim.mk>
2025-09-02 10:45:39 +0200

    add languages on logout

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -2,24 +2,31 @@ import React, { useEffect } from 'react';
 import '../styles/Logout.css'; 
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { useAuth } from '../context/AuthContext'; 
+import { useTranslation } from 'react-i18next';
 
 const Logout = () => {
     const { logout } = useAuth(); 
+    const { t } = useTranslation();
 
-     useEffect(() => {
-        logout(); 
+    useEffect(() => {
+        logout();
     }, [logout]); 
 
     return (
         <div className="logout-container">
             <HelmetProvider>
                 <Helmet>
-                    <title>Одјава</title>
+                    <title>{t('logoutTitle')}</title>
                 </Helmet>
             </HelmetProvider>
-            <h1>Успешно сте одјавени!</h1>
-            <p>Ви благодариме што ја искористивте нашата апликација.</p>
-            <button onClick={() => window.location.href = '/login'} className="login-button">Најави се повторно</button>
+            <h1>{t('logoutMessage')}</h1>
+            <p>{t('logoutThanks')}</p>
+            <button 
+                onClick={() => window.location.href = '/login'} 
+                className="login-button"
+            >
+                {t('loginAgain')}
+            </button>
         </div>
     );
 };
