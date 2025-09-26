15f8d08ed7fe001178842a6259308975236c1e16
DavidGjorgjievski <david.gjorgjievski@students.finki.ukim.mk>
2025-09-26 13:39:25 +0200

    unauthorized page design update

--- src/pages/Unauthorized.js
+++ src/pages/Unauthorized.js
@@ -1,17 +1,24 @@
 import { Link } from 'react-router-dom';
 import { useTranslation } from "react-i18next";
 import '../styles/Unauthorized.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 
 const Unauthorized = () => {
     const { t } = useTranslation();
 
     return (
-        <div>
-            <h1>{t("unauthorized.title")}</h1>
-            <p>{t("unauthorized.message")}</p>
-            <Link to="/" className="unauthorized-back-button">
-                {t("unauthorized.backButton")}
-            </Link>
+        <div className="unauthorized-container">
+            <div className="unauthorized-content">
+                <h1>
+                    {t("unauthorized.title")}  
+                    <FontAwesomeIcon icon={faTriangleExclamation} className="unauthorized-icon" />
+                </h1>
+                <p>{t("unauthorized.message")}</p>
+                <Link to="/" className="unauthorized-back-button">
+                    {t("unauthorized.backButton")}
+                </Link>
+            </div>
         </div>
     );
 };
